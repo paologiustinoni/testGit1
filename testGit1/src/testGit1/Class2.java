c7df2b695748fda3c46a2cd690b52bbf31efe9a4
Paolo Giustinoni <p.giustinoni@gmail.com>
2022-04-25 09:57:27 +0200

    commit 2

---
+++ testGit1/src/testGit1/Class2.java
@@ -0,0 +1,25 @@
+package testGit1;
+
+public class Class2 {
+	private double d1;
+	
+	private double d2;
+
+	public double getD1() {
+		return d1;
+	}
+
+	public void setD1(double d1) {
+		this.d1 = d1;
+	}
+
+	public double getD2() {
+		return d2;
+	}
+
+	public void setD2(double d2) {
+		this.d2 = d2;
+	}
+	
+	
+}
